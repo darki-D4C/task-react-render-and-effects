4464629d8ef088e4c63fd996d6baabc96c80c2b2
darki-D4C <den.pashmentov75@yandex.ru>
2022-06-07 21:13:04 +0300

    task is done

--- src/Effects.tsx
+++ src/Effects.tsx
@@ -1,5 +1,25 @@
 import { subscribe, unsubscribe } from './resources/API';
+import { useEffect, useState } from 'react';
 
 export function Effects(props: { sourceId: string }) {
-    return <div>123</div>;
+    const [lastMessage, setMessage] = useState(-1);
+
+    const callback = (messageID: number) => {
+        setMessage(messageID);
+    };
+
+    useEffect(() => {
+        subscribe(props.sourceId, callback);
+        return () => {
+            setMessage(-1);
+            unsubscribe(props.sourceId, callback);
+        };
+    }, [props.sourceId]);
+
+    return (
+        <div>
+            {props.sourceId}: {lastMessage}
+        </div>
+    );
 }
+© 2022 GitHub, Inc.
